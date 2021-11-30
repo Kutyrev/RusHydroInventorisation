6bcf443a30e13d71ef7bbcb99a65ebcadcb8a388
Kutyrev <proxium@gmail.com>
2021-11-30 14:40:48 +0300

    Secure pass summary and input

--- app/src/main/java/com/github/kutyrev/rushydroinventorisation/PreferenceFragmentCompat.kt
+++ app/src/main/java/com/github/kutyrev/rushydroinventorisation/PreferenceFragmentCompat.kt
@@ -1,12 +1,34 @@
 package com.github.kutyrev.rushydroinventorisation
 
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.PasswordTransformationMethod
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 
-
 class SettingsFragment : PreferenceFragmentCompat() {
+
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
+        val etpPassword : EditTextPreference? = preferenceManager.findPreference("service_pass")
+
+        etpPassword?.setOnBindEditTextListener {
+            it.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            it.transformationMethod = PasswordTransformationMethod.getInstance()
+            it.selectAll()
+        }
+
+        etpPassword?.setOnPreferenceChangeListener {
+                preference, newValue ->
+            if (newValue != "") etpPassword.summary = "***"
+            else etpPassword.summary = ""
+
+            true
+        }
+
+       if (!etpPassword?.text.isNullOrEmpty())etpPassword?.summary = "***"
+
     }
+
 }
